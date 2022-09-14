a48806dc67c383739bf34693e16557175ac8e4ac
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-13 19:18:58 -0700

    Remove Default Create React & Add Branding
    Remove the default Create React and add branding so that the application
    has a look and fill that is not the default version that came from the
    application's creation.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,9 @@
-import logo from './logo.svg'
-import './App.css'
 import { ThemeProvider } from './theme'
 
-function App() {
-  return (
-    <ThemeProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ThemeProvider>
-  )
-}
+const App = () => (
+  <ThemeProvider>
+    <h1>Comments</h1>
+  </ThemeProvider>
+)
 
 export default App
