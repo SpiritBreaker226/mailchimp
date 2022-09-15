c053f3fbb94a359cd2354bfbe61d4139a0f5b8cb
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-14 18:52:11 -0700

    Add App-Wide Styles
    Add some App-Wide styles so that the application looks somewhat decent.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,50 @@
+import styled from 'styled-components'
+
 import ErrorBoundary from './Components/ErrorBoundary'
 import { AddComment } from './AddComment'
 import { Comment } from './Comments'
 import { ThemeProvider } from './theme'
 
+const AppBody = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  padding: 2rem 0;
+`
+
+const AppHeader = styled.h1`
+  margin: 0 0 2rem;
+`
+
+const AppBodyContent = styled.section`
+  padding: 0.75rem 1.5rem;
+  border-radius: 1em;
+  box-shadow: 0 0 0 0.0625rem ${(props) => props.theme.text};
+  margin-bottom: 2rem;
+`
+
 const App = () => (
   <ErrorBoundary>
     <ThemeProvider>
-      <h1>Comments Feed</h1>
+      <AppBody>
+        <AppHeader>Comments Feed</AppHeader>
 
-      <AddComment />
+        <AppBodyContent>
+          <AddComment />
+        </AppBodyContent>
 
-      <Comment
-        remark={{
-          id: 1,
-          name: 'Billy Bob',
-          message: 'This is more then I can handle',
-          created: new Date(),
-        }}
-      />
+        <AppBodyContent>
+          <Comment
+            remark={{
+              id: 1,
+              name: 'Billy Bob',
+              message: 'This is more then I can handle',
+              created: new Date(),
+            }}
+          />
+        </AppBodyContent>
+      </AppBody>
     </ThemeProvider>
   </ErrorBoundary>
 )
